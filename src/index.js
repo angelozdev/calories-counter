b4baa5dfff461130330115c93627d068404c529c
Angelo Zambrano <angelozam17@gmail.com>
2020-06-07 10:51:17 -0500

    index.html: Done

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const $btn = document.querySelector('#btn');
+
+$btn.addEventListener('click', () => {
+   const $description = document.querySelector('#description');
+   const $carbs = document.querySelector('#carbs');
+   const $calories = document.querySelector('#calories');
+   const $proteins = document.querySelector('#proteins');
+
+   console.log($description.value);
+   
+})
+
+
+
