ad3c5b7526a2d69c62da3f84d62a380ca599c01d
Angelo Zambrano <angelozam17@gmail.com>
2020-06-07 19:43:11 -0500

    Comprendido

--- src/index.js
+++ src/index.js
@@ -10,25 +10,37 @@ const $proteins = document.querySelector('#proteins');
 
 const list = []
 
-const attrsToString = (obj = {}) => {
-   const keys = Object.keys(obj)
-   const attrs = []
+const attrsToString = (attrs) => {
+   const keys = Object.keys(attrs);
+   const attributes = [];
 
    keys.forEach(attr => {
-      attrs.push(`${attr}="${obj[attr]}"`)
+      attributes.push(`${attr}="${attrs[attr]}"`)
    });
 
-   const string = attrs.join(' ')
-
+   const string = attributes.join(' ')
    return string;
+} 
+
+const tagAttrs = (obj = {}) => (content = '') => (
+   `<${obj.tag}${obj.attrs ? ' ' : ''}${obj.attrs ? attrsToString(obj.attrs) : ''}>${content}</${obj.tag}>`
+)
+
+
+const tag = (t) => {
+   if(typeof t === 'string'){
+      return tagAttrs({tag: t})
+   }else{
+      return tagAttrs(t)
+   }
 }
 
+const tableRowTag = tag('tr');
+const tableRow = (items) => tableRowTag(tableCells(items))
 
-const tagAttrs = (obj) => (content = "") => (
-   `<${obj.tag}${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
-   )
-   
-console.log(tagAttrs({tag: 'h1', attrs: {class: 'hola'}})('Hola mundo'));
+const tableCell = tag({tag: 'td', attrs: {class: 'border px-4 py-2'}});
+const tableCells = (items = []) => items.map(tableCell).join('\n')
+
 
 /* FUNCTIONS */
 const validateInput = (element) => {
@@ -60,6 +72,7 @@ const validateInputs = () => {
 }
 
 const addObject = () => {
+   const $items = document.querySelector('#items')
    const newItem = {
       description: $description.value,
       calories: Number($calories.value),
@@ -68,9 +81,9 @@ const addObject = () => {
    }
 
    list.push(newItem)
-
+   $items.innerHTML += tableRow(Object.values(newItem))
    cleanInputs()
-   console.log(tag('h1')(newItem.description));
+   console.log(tableRow(Object.values(newItem)));
 }
 
 const cleanInputs = () => {
